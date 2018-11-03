e23d4e2dcba13da0e4fb7c0934f8d6dd6f2cbba3
Unknown <claytonmatulich13@gmail.com>
2018-11-02 20:04:55 -0700

    init

---
+++ src/UsernameForm.js
@@ -0,0 +1,58 @@
+// ./src/UsernameForm.js
+
+import React, { Component } from 'react'
+import { TextInput } from 'react-desktop/macOs'
+import { Button } from 'react-desktop/macOs'
+
+class UsernameForm extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: ''
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.handleSubmit(this.state.username)
+  }
+
+  handleChange = e => {
+    this.setState({ username: e.target.value })
+  }
+
+  render() {
+    return (
+      <div className="username-form">
+        <form onSubmit={this.handleSubmit} id='loginForm'>
+
+          <div id='titleWrapper'>
+            <label id = "title" > Stardust </label>
+          </div>
+
+          <div id = "inputFieldWrapper">
+            <input 
+              id="inputField"
+              type = "text"
+              label="Username:"
+              placeholder="     For example, @l337H4ck0r"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div id = "submitButtonWrapper">
+            {/* <Button color="blue" type="submit">
+              Submit
+            </Button> */}
+
+            <input id="submitButton" type = "submit" >
+            
+            </input>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default UsernameForm
